22b674328c64c1085953dc100e88a392dc5b1d5f
Ryuchen <chenhaom1993@hotmail.com>
2022-02-25 11:25:59 +0800

    Update virt-manager.rb

--- virt-manager.rb
+++ virt-manager.rb
@@ -11,15 +11,19 @@ class VirtManager < Formula
   depends_on "gettext" => :build
 
   depends_on "adwaita-icon-theme"
+  depends_on "hicolor-icon-theme"
+  depends_on "docutils"
   depends_on "gtk+3"
   depends_on "gtk-vnc"
   depends_on "gtksourceview4"
   depends_on "libosinfo"
+  depends_on "osinfo-db"
   depends_on "libvirt-glib"
   depends_on "libxml2"
-  depends_on "osinfo-db"
   depends_on "python"
   depends_on "spice-gtk"
+  depends_on "py3cairo"
+  depends_on "pygobject3"
   depends_on "vte3"
 
   resource "certifi" do
@@ -42,16 +46,6 @@ class VirtManager < Formula
     sha256 "0245c226d7b83b32449299d0ca5f1f250dcc07edf9f2fcd87cb7462f09e4c026"
   end
 
-  resource "pycairo" do
-    url "https://files.pythonhosted.org/packages/bc/3f/64e6e066d163fbcf13213f9eeda0fc83376243335ea46a66cefd70d62e8f/pycairo-1.20.1.tar.gz"
-    sha256 "1ee72b035b21a475e1ed648e26541b04e5d7e753d75ca79de8c583b25785531b"
-  end
-
-  resource "PyGObject" do
-    url "https://files.pythonhosted.org/packages/cc/72/48cfdd7a0caf3c27f392d2657731ac6f7c3c1c0a60bfeba3e1ba9ffa7ba9/PyGObject-3.42.0.tar.gz"
-    sha256 "b9803991ec0b0b4175e81fee0ad46090fa7af438fe169348a9b18ae53447afcd"
-  end
-
   resource "requests" do
     url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
     sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
@@ -67,6 +61,7 @@ class VirtManager < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/python", "-m", "pip", "install", "-U", "pip"
     venv.pip_install resources
 
     # virt-manager uses distutils, doesn't like --single-version-externally-managed
@@ -94,18 +89,17 @@ class VirtManager < Formula
   end
 end
 __END__
-diff --git a/virtManager/virtmanager.py b/virtManager/virtmanager.py
-index f6d538b..1f01aa5 100755
---- a/virtManager/virtmanager.py
-+++ b/virtManager/virtmanager.py
-@@ -136,8 +136,8 @@ def parse_commandline():
-     parser.add_argument("--debug", action="store_true",
+diff --git a/virt-manager b/virt-manager
+index 15d5109..8ee305a 100755
+--- a/virt-manager
++++ b/virt-manager
+@@ -151,7 +151,8 @@ def parse_commandline():
          help="Print debug output to stdout (implies --no-fork)",
          default=False)
--    parser.add_argument("--no-fork", action="store_true",
+     parser.add_argument("--no-fork", action="store_true",
 -        help="Don't fork into background on startup")
-+    parser.add_argument("--fork", dest='no_fork', action="store_false",
-+        help="Fork into background on startup")
++        help="Don't fork into background on startup",
++        default=True)
 
      parser.add_argument("--show-domain-creator", action="store_true",
          help="Show 'New VM' wizard")
